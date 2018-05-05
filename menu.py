02f59a261f3d9ecb855291cbc3e97ae07b3b405f
Jesus <sandoval14yamir@hotmail.com>
2018-05-05 17:30:27 -0600

    Cambie la letra del titulo

--- menu.py
+++ menu.py
@@ -13,7 +13,7 @@ def load_interface(xPoss, yPoss, xWidth, fgColor, bgColor, fonts):
     mainCanvas = Canvas(main, width=W2, height=H2, bg=bgColor)
     mainCanvas.place(x=xPoss, y=yPoss)
 
-    tittleLabel = Label(mainCanvas, text='Pong', font=fonts, fg=fgColor, bg=bgColor, width=xWidth, justify=RIGHT)
+    tittleLabel = Label(mainCanvas, text='Pong', font="Nokia Cellphone", fg=fgColor, bg=bgColor, width=xWidth, justify=RIGHT)
     tittleLabel.place(x=xPoss + 20, y=yPoss + 20)
 
     playersLabel = Label(mainCanvas, text='Players', font=fonts, fg=fgColor, bg=bgColor, width=xWidth, justify=RIGHT)
