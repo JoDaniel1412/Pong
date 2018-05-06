6c98c60bf63a9ed5aba058ac51869457418bb047
Jesus <sandoval14yamir@hotmail.com>
2018-05-05 23:34:11 -0600

    Termine el menu incicial los botones y el resto de interfaz Agrego la imagen para el fondo Actualizo la version mas reciente del juego y el menu

--- menu.py
+++ menu.py
@@ -9,9 +9,18 @@ main.minsize(W2, H2)
 main.resizable(NO, NO)
 Fondo_pong = PhotoImage(file="img/Imagen de menu de pong.gif")
 
-
 # Interface
 def load_interface(xPoss, yPoss, xWidth, fgColor, bgColor, fonts):
+    #Ventana de ajustes
+    def ajustes():
+        main.withdraw()
+        ventana2 = Toplevel()
+        ventana2.title("Ajustes")
+        ventana2.minsize(W2, H2)
+        ventana2.resizable(width=NO, height=NO)
+        canvas2 = Canvas(ventana2, width=W2, height=H2, bg="black")
+        canvas2.place(x=-1, y=0)
+
     mainCanvas = Canvas(main, width=W2, height=H2, bg=bgColor)
     mainCanvas.place(x=xPoss, y=yPoss)
 
@@ -24,7 +33,7 @@ def load_interface(xPoss, yPoss, xWidth, fgColor, bgColor, fonts):
     playersButton2 = Button(mainCanvas, text='2 Player', font=fonts + str(30), fg=fgColor, bg=bgColor, width=xWidth,justify=RIGHT, borderwidth=0)
     playersButton2.place(x=xPoss + 290, y=yPoss + 310)
 
-    playersOption = Button(mainCanvas, text='Options', font=fonts + str(30), fg=fgColor, bg=bgColor, width=xWidth,justify=RIGHT, borderwidth=0)
+    playersOption = Button(mainCanvas, text='Options', font=fonts + str(30), fg=fgColor, bg=bgColor, width=xWidth,justify=RIGHT, borderwidth=0, command=ajustes)
     playersOption.place(x=xPoss + 290, y=yPoss + 370)
 
     playersHightscore = Button(mainCanvas, text='Scores', font=fonts + str(30), fg=fgColor, bg=bgColor, width=xWidth,justify=RIGHT, borderwidth=0)
