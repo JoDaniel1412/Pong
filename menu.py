8047d575e0faeaf2ddc2d7660812208ecf27db23
Jesus <sandoval14yamir@hotmail.com>
2018-05-06 15:05:50 -0600

    resubida por seguridad de los codigos del menu y el juego en interfaz Adelanto de la creacion de la tabla de scores e ingreso de jugadores

--- menu.py
+++ menu.py
@@ -78,10 +78,96 @@ def load_interface(xPoss, yPoss, xWidth, fgColor, bgColor, fonts):
 
         volver =  Button(canvas2, text = "VOLVER", font = fonts +str(20),  fg="black", bg="White", borderwidth=0, command = cerrar_ajustes)
         volver.place(x = 10, y = 10)
-    #def puntuaciones():
+    def puntuaciones():
+            def cerrar_puntuciones():
+                main.deiconify()
+                puntuaciones.destroy()
 
+            main.withdraw()
 
+            ventana_scores = Toplevel()
+            ventana_scores.title("Ingreso de Jugador")
 
+            ventana_scores.minsize(W2, H2)
+            ventana_scores.resizable(width=NO, height=NO)
+
+            canvas_scores = Canvas(ventana_scores, width=W2, height=H2, bg="black")
+            canvas_scores.place(x=-1, y=0)
+
+            label_scores = Label(canvas_scores, text ="Digite nombre de jugador:", font = fonts +str(20), fg=fgColor, bg=bgColor)
+            label_scores.place(x=40, y=120)
+
+            escribir_jugadores = Entry(canvas_scores, width=30)
+            escribir_jugadores.place(x=200, y=300)
+
+            def agregar_scores():
+                agregar_puntuaciones = open('Scores.txt', 'a')
+                agregar_puntuaciones.write = (escribir_jugadores.get())
+    def mostrar_puntuaciones():
+        def cerrar_mostrar_puntuciones():
+            main.deiconify()
+            ventana__mostrar_scores.destroy()
+        main.withdraw()
+
+        ventana__mostrar_scores = Toplevel()
+        ventana__mostrar_scores.title("Puntuaciones")
+
+        ventana__mostrar_scores.minsize(W2, H2)
+        ventana__mostrar_scores.resizable(width=NO, height=NO)
+
+        canvas_mostrar_scores = Canvas(ventana__mostrar_scores, width=W2, height=H2, bg="black")
+        canvas_mostrar_scores.place(x=-1, y=0)
+
+        label_mejores = Label(canvas_mostrar_scores, text ="Mejores Puntuaciones:", font = fonts +str(20), fg=fgColor, bg=bgColor)
+        label_mejores.place(x=200, y=120)
+
+        canvas_tabla = Canvas(canvas_mostrar_scores, width=W2//2, height=H2//2)
+        canvas_tabla.place(x=200, y=200)
+
+        cerrar_scores = Button(canvas_mostrar_scores, text = "BACK!", font = fonts +str(20),  fg="black", bg="White", borderwidth=0, command=cerrar_mostrar_puntuciones)
+        cerrar_scores.place(x = 5, y = 0)
+
+        def separarPuntuaciones(i):
+            if i == len(listaScores):
+                return
+            listaScores[i] = listaScores[i].replace("\n", "").split(";")
+            separarPuntuaciones(i + 1)
+
+        file = open("Scores.txt", 'r')
+        listaScores = file.readlines()
+        separarPuntuaciones(0)
+        file.close()
+        def crearTabla(x, y, columns):
+            global tabla
+            if x == len(listaScores):
+                return ''
+            elif y == 2:
+                tabla += [columns]
+                return crearTabla(x + 1, 0, [])
+            else:
+                if y == 0:
+                    seccion = Entry(canvas_tabla, text='', width=3, justify=CENTER)
+                    seccion.grid(row=x, column=y)
+                    return crearTabla(x, y + 1, columns + [seccion])
+                seccion = Entry(canvas_tabla, text='', width=30)
+                seccion.grid(row=x, column=y)
+                return crearTabla(x, y + 1, columns + [seccion])
+
+        # Llena la tabla anteriormente creada
+        def llenarTabla(x, y):  # funcion que llena la tabla para vendedores con el vendedores.txt
+            global tabla
+            if x == len(listaScores):
+                return
+            elif y != 2:
+                print(listaScores[x][y])
+                tabla[x][y].insert(0, listaScores[x][y])
+                return llenarTabla(x, y + 1)
+            else:
+                return llenarTabla(x + 1, 0)
+
+        crearTabla(0, 0, [])
+        llenarTabla(0, 0)
+        tabla = []
 
 
 
@@ -101,7 +187,7 @@ def load_interface(xPoss, yPoss, xWidth, fgColor, bgColor, fonts):
     playersOption = Button(mainCanvas, text='Options', font=fonts + str(30), fg=fgColor, bg=bgColor, width=xWidth,justify=RIGHT, borderwidth=0, command=ajustes)
     playersOption.place(x=xPoss + 290, y=yPoss + 370)
 
-    playersHightscore = Button(mainCanvas, text='Scores', font=fonts + str(30), fg=fgColor, bg=bgColor, width=xWidth,justify=RIGHT, borderwidth=0)
+    playersHightscore = Button(mainCanvas, text='Scores', font=fonts + str(30), fg=fgColor, bg=bgColor, width=xWidth,justify=RIGHT, borderwidth=0, command=mostrar_puntuaciones)
     playersHightscore.place(x=xPoss + 290, y=yPoss + 430)
 
 
