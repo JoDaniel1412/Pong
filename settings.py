349e043109425dd7e5198c15ad391035c1094184
JoDaniel1412 <jodaniel1412@gmail.com>
2018-05-23 12:27:49 -0600

    Reestructuracion de los ajustes como variables.

---
+++ settings.py
@@ -0,0 +1,26 @@
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+
+# Variables
+W, H = 1400, 800
+W1, H1 = 800, 600
+W2, H2 = 320, 870
+HW, HH = W // 2, H // 2
+FPS = 60
+secs = 3
+
+# Default Game Variables
+pallets_selected = 1
+difficulty_selected = 1
+style_selected = 0
+players_selected = 1
+top_points = 5
+
+# Players controls
+player1_keys = ('py.K_w', 'py.K_s')
+player2_keys = ('py.K_UP', 'py.K_DOWN')
